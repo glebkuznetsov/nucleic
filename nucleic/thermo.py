d3feeb39ace844a72d34f9cd4dd0ce016e35fe50
benpruitt <bpruittvt@gmail.com>
2014-01-03 13:18:14 -0500

    minor changes to thermo.py

--- nucleic/thermo.py
+++ nucleic/thermo.py
@@ -6,29 +6,32 @@ Thermodynamic constants and calculations.
 
 '''
 
-import math.e
+import math
 
-R = 1.9872e-3			# Gas constant (kcal -1 / mol)
+R = 1.9872e-3  # Gas constant (1 / kcal * mol)
 
 
 def c_to_k(deg_c):
-	''' Convert degrees Celsius to degrees Kelvin ''' 
-	return deg_c − 273.15
+    ''' Convert degrees Celsius to degrees Kelvin ''' 
+    return deg_c - 273.15
 
 
 def k_to_c(deg_k): 
-	''' Convert degrees Kelvin to degrees Celsius ''' 
-	return deg_k + 273.15 
+    ''' Convert degrees Kelvin to degrees Celsius ''' 
+    return deg_k + 273.15 
 
 
 def calc_ka(dg, deg_c):
-	''' Return the association constant at a given temperature '''
-	return math.e**(-dg/(R * c_to_k(deg_c)))
-
-
-def calc_rand_coil(dg, deg_c):
-	''' Return the percent of randomly coiled oligo with dg at deg_c degrees '''
-	return 1/(ka(dg, deg_c) + 1)
+    ''' Return the association constant at a given temperature '''
+    return math.e**(-dg/(R * c_to_k(deg_c)))
 
 
 def calc_dg(ds, dh, deg_c):
+    ''' Return the dg at a given temp using the provided ds and dh '''
+    return dh - ds * c_to_k(deg_c)
+
+
+def calc_rand_coil(dg, deg_c):
+    ''' Return the percent of randomly coiled oligo with dg at deg_c degrees '''
+    return 1/(calc_ka(dg, deg_c) + 1)
+
